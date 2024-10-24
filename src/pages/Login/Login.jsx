6fce4268c4aa81aadf4557cd8748ffbe7aa5124d
Shahnawaz Sazid <shahnawazsazid60@gmail.com>
2024-10-24 13:19:26 +0600

    Login System with Reset Passsword System Added

--- src/pages/Login/Login.jsx
+++ src/pages/Login/Login.jsx
@@ -1,7 +1,58 @@
-import { Link } from 'react-router-dom'
+
 import { FcGoogle } from 'react-icons/fc'
+import { Link, useNavigate } from 'react-router-dom'
+
+import toast from 'react-hot-toast'
+import { TbFidgetSpinner } from "react-icons/tb";
+import useAuth from '../../hooks/useAuth';
+import { useState } from 'react';
 
 const Login = () => {
+  const navigate = useNavigate()
+  const { signIn, signInWithGoogle, resetPassword, loading, setLoading } = useAuth()
+  const [email, setEmail] = useState('')
+
+  const handleSubmit = async e => {
+    e.preventDefault()
+    const form = e.target
+    const email = form.email.value
+    const password = form.password.value
+
+    try {
+      setLoading(true)
+      // 1. upload image and get url
+      await signIn(email, password)
+      navigate('/')
+      toast.success('Sign in Successful')
+    } catch (error) {
+      console.log(error)
+      toast.error(error.message)
+      setLoading(false)
+    }
+  }
+
+  const handleResetPassword = async () => {
+    if(!email) return toast.error('Please Provide email')
+    try {
+      await resetPassword(email)
+      toast.success('Request Successful ')
+      setLoading(false)
+    } catch (error) {
+      console.log(error)
+      toast.error(error.message)
+      setLoading(false)
+    }
+  }
+  // handle google signIn
+  const handleGoogleSignIn = async () => {
+    try {
+      await signInWithGoogle()
+      navigate('/')
+    } catch (error) {
+      console.log(error)
+      toast.error(error.message)
+    }
+  }
   return (
     <div className='flex justify-center items-center min-h-screen'>
       <div className='flex flex-col max-w-md p-6 rounded-md sm:p-10 bg-gray-100 text-gray-900'>
@@ -12,6 +63,7 @@ const Login = () => {
           </p>
         </div>
         <form
+          onSubmit={handleSubmit}
           noValidate=''
           action=''
           className='space-y-6 ng-untouched ng-pristine ng-valid'
@@ -25,6 +77,7 @@ const Login = () => {
                 type='email'
                 name='email'
                 id='email'
+                onBlur={e=>setEmail(e.target.value)}
                 required
                 placeholder='Enter Your Email Here'
                 className='w-full px-3 py-2 border rounded-md border-gray-300 focus:outline-rose-500 bg-gray-200 text-gray-900'
@@ -54,12 +107,12 @@ const Login = () => {
               type='submit'
               className='bg-rose-500 w-full rounded-md py-3 text-white'
             >
-              Continue
+              {loading ? <TbFidgetSpinner className='animate-spin mx-auto' /> : 'Continue'}
             </button>
           </div>
         </form>
         <div className='space-y-1'>
-          <button className='text-xs hover:underline hover:text-rose-500 text-gray-400'>
+          <button onClick={handleResetPassword} className='text-xs hover:underline hover:text-rose-500 text-gray-400'>
             Forgot password?
           </button>
         </div>
@@ -70,11 +123,11 @@ const Login = () => {
           </p>
           <div className='flex-1 h-px sm:w-16 dark:bg-gray-700'></div>
         </div>
-        <div className='flex justify-center items-center space-x-2 border m-3 p-2 border-gray-300 border-rounded cursor-pointer'>
+        <button disabled={loading} onClick={handleGoogleSignIn} className='flex justify-center items-center space-x-2 border m-3 p-2 border-gray-300 border-rounded disabled:cursor-not-allowed cursor-pointer'>
           <FcGoogle size={32} />
 
-          <p>Continue with Google</p>
-        </div>
+          <p>Sign In with Google</p>
+        </button>
         <p className='px-6 text-sm text-center text-gray-400'>
           Don&apos;t have an account yet?{' '}
           <Link
